eebfb9e48e3765b66a9e4a3faaf83f3ef2a7cf6e
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-04-23 22:20:26 -0400

    added columns for product tag, also synced sequelize models to db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const db = require('./config/connection');
 // import sequelize connection
 
 const app = express();
@@ -11,6 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+db.sync().then(() => {
+    app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
