835557e015230df9d156f35c7aa822735a1de3d3
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-04-25 17:45:54 -0400

    fixed typo causing product tags to not display

--- models/index.js
+++ models/index.js
@@ -9,9 +9,9 @@ Product.belongsTo(Category);
 // Categories have many Products
 Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {through: 'ProductTag'});
+Product.belongsToMany(Tag, {through: ProductTag});
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {through: 'ProductTag'});
+Tag.belongsToMany(Product, {through: ProductTag});
 
 module.exports = {
   Product,
